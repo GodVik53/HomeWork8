5442edba301fffe0382c2a49b48abc5959ef8886
GodVik53 <137499699+GodVik53@users.noreply.github.com>
2023-08-01 09:53:48 +0300

    Update Task_62.cs
    решили задачу № 62

--- Task_62.cs
+++ Task_62.cs
@@ -6,50 +6,54 @@
 // 10 09 08 07
 
 
+//Задаем размеры массива
+    Console.WriteLine ("Введите размеры квадратного массива X x X");
+    Console.Write ("X = ");
+    int x = Convert.ToInt32(Console.ReadLine());
 
+// Задаем массив и заполняем его по спирали 
+    int [,] nums = new int [x,x];
+    int num = 1;
+    int i = 0;
+    int j = 0;
+    
+    while (num <= nums.GetLength(0)*nums.GetLength(1)) {
+        nums [i,j]=num;
+        num=num+1;
+          if (i <= j + 1 && i + j < nums.GetLength(1) - 1){
+            j++;
+          }
+            else { 
+                if (i < j && i + j >= nums.GetLength(0) - 1) {
+                 i++;
+                }
+                else {
+                    if (i >= j && i + j > nums.GetLength(1) - 1) {
+                        j--;
+                    }    
+                    else {
+                        i--;
+                    }
+                }
+            }    
 
-int n = 4;
-int[,] sqareMatrix = new int[n, n];
-
-
-int temp = 1;
-int i = 0;
-int j = 0;
-
-
-while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
-{
-  sqareMatrix[i, j] = temp;
-  temp++;
-  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
-    j++;
-  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
-    i++;
-  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
-    j--;
-  else
-    i--;
-}
-
-
-WriteArray(sqareMatrix);
-
-
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
-
-
-      else Console.Write($"{array[i,j]} ");
+        
     }
-    Console.WriteLine();
-  }
-}
+// Выводим результат
+
+    for (int k = 0; k < nums.GetLength(0); k++) {
+        for (int l = 0; l < nums.GetLength(1); l++) {
+               if (nums[k,l]/10<1) {
+                   Console.Write($"0{nums[k,l]} ");
+               }
+               else {
+                   Console.Write($"{nums[k,l]} ");
+               }
+            }
+        Console.WriteLine();
+        }    
+        
+
 
 
 
